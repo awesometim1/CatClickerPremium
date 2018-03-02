acfda98a040cc80f3543fc8d5317e2387df9ef14
Tim Lee <awesometim1@gmail.com>
2018-03-01 22:55:49 -0500

    fix: Fix score incrementing incorrectly and catCopy not updating

--- js/mvo.js
+++ js/mvo.js
@@ -16,10 +16,16 @@ $(function(){
                 localStorage.cats = JSON.stringify(models);
             }
         },
-        addScore: function(kitty) {
-            var data = JSON.parse(localStorage.cats);
-            console.log(data);
-            localStorage.cats = JSON.stringify(data);
+        addScore: function(id) {
+            var cats = JSON.parse(localStorage.cats);
+            for (var i = 0; i < cats.length; i ++){
+                var catName = "kitten" + id;
+                if (cats[i]['catName']==catName){
+                    cats[i]['catScore']++;
+                    break;
+                }
+            }
+            localStorage.cats = JSON.stringify(cats);
         },
         getModel: function() {
             return JSON.parse(localStorage.cats);
@@ -45,8 +51,16 @@ $(function(){
             model.init();
             view.init();
         },
-        incScore: function(kitty){
-            model.addScore(kitty);
+        incScore: function(id){
+            model.addScore(id);
+        },
+        matchCat: function(catName){
+            var cats = model.getModel();
+            for (var i = 0; i < cats.length; i ++){
+                if (cats[i]['catName']==catName){
+                    return cats[i];
+                }
+            };//end for
         }
     };
 
@@ -73,22 +87,29 @@ $(function(){
                 //Select all of the buttons by id and add eventlistener onClick()
                 $('#' + "kitten" + (i+1)).click((function(catCopy){
                     return function(){
+                        if ($('h1').length > 1){
+                            $('h1')[0].remove();
+                            $('h1')[0].remove();
+                        }
                         $('.cat-display').prepend('<h1>' + catCopy.catName + '</h1>')
                         $('.display').attr('src', catCopy.catLink);
                         $('.cat-display').append('<h1 class = "score">' + catCopy.catScore + '</h1>');
+                        catCopy = octopus.matchCat(catCopy.catName);
+                        
                     }
                     
                 })(cat));
 
-            };
+            }; //end for
             $('.display').click(function(){
                 var idNum = parseInt($('.display').attr("src").slice(17));
                 octopus.incScore(idNum);
                 var score = $('.score').html();
                 $('.score').html("" + (parseInt(score) + 1));
+                
             });
-        }
-    };
+        }//end render
+    };//end view
 
     octopus.init();
 
